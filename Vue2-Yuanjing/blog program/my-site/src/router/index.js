558a7f06dcb1b2bfae7712ea012c402d02634962
qitao chen <trace370@gmail.com>
2021-08-24 12:46:41 +0930

    only test new computer upload file to git functionality

--- Vue2-Yuanjing/blog program/my-site/src/router/index.js
+++ Vue2-Yuanjing/blog program/my-site/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter);//使用一个vue插件
 import routes from "./routes.js"
-
+//test new computer config function 
 const router = new VueRouter({
   //配置
   routes,
