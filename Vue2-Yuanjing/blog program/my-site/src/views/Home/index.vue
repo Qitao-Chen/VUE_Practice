8ea24de9749b50ba7cfd1fbabb3b0b2d5d151468
qitao chen <trace370@gmail.com>
2021-09-06 01:16:43 +0930

    home conponent part1 completed---index.vue

--- Vue2-Yuanjing/blog program/my-site/src/views/Home/index.vue
+++ Vue2-Yuanjing/blog program/my-site/src/views/Home/index.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home-container" v-if="banners.length > 0">
-    <ul>
-      <li v-for="item in banners" :key="item.id">
+  <div class="home-container" ref="container">
+    <ul
+      class="carousel-container"
+      :style="{
+        marginTop,
+      }"
+    >
+      <li v-for="item in banners" :key="item.id"
+      :style="{
+        background,
+      }"
+      >
         <Carouselitem />
       </li>
     </ul>
-    <div class="icon icon-up">
+    <div class="icon icon-up"
+    v-show="index >= 1"
+    @click="switchTo(index - 1)"
+    >
       <Icon type="arrowUp" />
     </div>
-    <div class="icon icon-down">
+    <div class="icon icon-down"
+    v-show="index < banners.length-1"
+     @click="switchTo(index + 1)"
+    >
       <Icon type="arrowDown" />
     </div>
     <ul class="indicator">
-      <li v-for="item in banners" :key="item.id"></li>
+      <li :class="{active: i === index }" v-for="(item,i) in banners" :key="item.id"
+      @click="switchTo(i)"
+      ></li>
     </ul>
   </div>
 </template>
@@ -30,12 +47,35 @@ export default {
   data() {
     return {
       banners: [],
+      index: 0, //当前显示地基张轮播图
+      containerHeight: 0, //整个容器的高度
+      
     };
   },
   async created() {
     this.banners = await getBanners();
     console.log(this.banners);
   },
+  mounted() {
+    this.containerHeight = this.$refs.container.clientHeight;
+  },
+  computed: {
+    marginTop() {
+      console.log(this.containerHeight);
+      return -this.index * this.containerHeight + "px";
+    }, 
+    background(){
+      const color= ["lightgreen","red","lightblue"];
+      return color[this.index]
+    }
+  },
+  methods:{
+    switchTo(i){
+    
+      this.index = i;
+      
+    }
+  }
 };
 </script>
 
@@ -47,15 +87,28 @@ export default {
   width: 100%;
   height: 100%;
   position: relative;
+  background: @dark;
   ul {
+    
     margin: 0;
+    list-style: none;
+    padding: 0;
+  }
+  .carousel-container {
+    transition: 500ms;
+    width: 100%;
+    height: 100%;
+    li {
+      width: 100%;
+      height: 100%;
+    }
   }
   .icon {
     cursor: pointer;
     color: @gray;
     @distance: 25px;
     font-size: 30px;
-    .self-center();
+    .self-center(fixed);
     transform: translateX(-50%);
     &.icon-up {
       top: @distance;
@@ -87,6 +140,25 @@ export default {
       }
       100% {
         transform: translate(-50%, -@jump);
+      }
+    }
+  }
+  .indicator {
+    .self-center(fixed);
+    transform: translateY(-50%);
+    left: auto;
+    right: 20px;
+    li {
+      width: 7px;
+      height: 7px;
+      border-radius: 50%;
+      background: @words;
+      cursor: pointer;
+      margin-bottom: 10px;
+      border: 1px solid #fff;
+      box-sizing: border-box;
+      &.active {
+        background: #fff;
       }
     }
   }
